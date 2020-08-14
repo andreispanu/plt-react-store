7e425f5a7bb2154f7a3d53c051812f9d6edd4419
Andrei Tiberiu Spanu <andreispanu@gmail.com>
2020-08-14 17:14:37 +0100

    Overall styling

--- src/components/quantity.js
+++ src/components/quantity.js
@@ -1,92 +1,112 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   itemContainer: {
-    padding: theme.spacing(2)
+    padding: theme.spacing(2),
+
   },
   itemQuantity: {
-    width: '100%',
-    textAlign: 'center'
+    width: "100%",
+    textAlign: "center",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputContainer: {
+    '& input': {
+      textAlign: 'center',
+      fontSize: 20,
+      height: theme.spacing(5)
+    }
   },
   itemRemove: {
-    width: '100%',
-    textAlign: 'center'
-  }
+    width: "100%",
+    textAlign: "center",
+    marginTop: theme.spacing(1)
+  },
 }));
 
 const Quantity = (props) => {
   const classes = useStyles();
   const [quantity, setQuantity] = useState(0);
 
-
   // Increment basket
   const add = (price, id) => {
-    setQuantity(quantity => quantity + 1)
+    setQuantity((quantity) => quantity + 1);
     props.updateCart({
-      value: (quantity + 1) * price,
+      value: price,
       id,
-      action: "add"
-    })
-  }
+      action: "add",
+      quantity: (quantity + 1),
+    });
+  };
 
   // Remove items from basket
   const remove = (price, id) => {
     if (quantity === 0) {
-      setQuantity(0)
+      setQuantity(0);
     } else {
-      setQuantity(quantity => quantity - 1)
+      setQuantity((quantity) => quantity - 1);
       props.updateCart({
-        value: (quantity - 1) * price,
+        value: price,
         id,
-        action: "remove"
-      })
+        action: "remove",
+        quantity: (quantity - 1)
+      });
     }
-  }
+  };
 
   // Clear items from basket
   const clearItems = (id) => {
-    setQuantity(0)
+    setQuantity(0);
     props.updateCart({
       value: 0,
       id,
-      action: "cancel"
-    })
-  }
+      action: "cancel",
+      quantity: 0
+    });
+  };
 
   return (
     <div className={classes.itemContainer}>
       <div className={classes.itemQuantity} key={props.itemId}>
-
-        <IconButton aria-label="add" onClick={() => add(props.itemPrice, props.itemId)}>
+        <IconButton
+          aria-label="add"
+          onClick={() => add(props.itemPrice, props.itemId)}
+        >
           <AddCircleOutlineIcon />
         </IconButton>
 
-        <TextField
-          value={quantity}
-        />
+        <div >
+          <TextField value={quantity} className={classes.inputContainer} />
+        </div>
 
-        <IconButton aria-label="remove" onClick={() => remove(props.itemPrice, props.itemId)}>
+        <IconButton
+          aria-label="remove"
+          onClick={() => remove(props.itemPrice, props.itemId)}
+        >
           <RemoveCircleOutlineIcon />
         </IconButton>
       </div>
 
       <div className={classes.itemRemove}>
-        {quantity > 0 && <Button onClick={() => clearItems(props.itemId)}>Remove</Button>}
+        {quantity > 0 && (
+          <Button onClick={() => clearItems(props.itemId)}>Remove</Button>
+        )}
       </div>
     </div>
-
-  )
-}
+  );
+};
 
 Quantity.propTypes = {
-  itemId: PropTypes.number.isRequired
-}
+  itemId: PropTypes.number.isRequired,
+};
 
-export default Quantity
+export default Quantity;
--- src/containers/basket.js
+++ src/containers/basket.js
@@ -1,44 +1,94 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { DashboardContext } from '../context/Context';
+import React, { useState, useEffect, useContext } from "react";
+import { DashboardContext } from "../context/Context";
 
 // Material UI Components
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-
-import Quantity from '../components/quantity';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+// Components
+import Quantity from "../components/quantity";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    '& h6': {
+    "& h6": {
       fontSize: theme.spacing(2),
-      textTransform: 'uppercase',
+      textTransform: "uppercase",
+      fontWeight: theme.typography.fontWeightBold,
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  // Header
+  headerItems: {
+    padding: theme.spacing(2),
+    maxWidth: 960,
+    margin: 'auto',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  activeFilters: {
+    height: 30,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '& span': {
+      paddingLeft: theme.spacing(1),
       fontWeight: theme.typography.fontWeightBold
     }
   },
-  paper: {
-    padding: theme.spacing(2)
-  },
-  itemContainer: {
-    padding: theme.spacing(2)
-  },
-  imageContainer: {
-    '& img': {
-      display: 'inline-block',
-      width: '100%'
+  totalsContainerHeader: {
+    height: 30,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '& span': {
+      paddingLeft: theme.spacing(1),
+      fontWeight: theme.typography.fontWeightBold,
     }
   },
-  textContainer: {
+  content: {
+    maxWidth: 960,
+    margin: 'auto'
   },
+  right: {
+    height: '100%'
+  },
+  // Item
+  itemContainer: {
+    padding: theme.spacing(2),
+  },
+  imageContainer: {
+    "& img": {
+      display: "inline-block",
+      width: "100%",
+    },
+  },
+  // Dropdown Color Selector
   formControl: {
     width: "100%",
+    boxSizing: "border-box",
+    padding: `${theme.spacing(4)}px 0 0 ${theme.spacing(4)}px`
+  },
+  formControlItem: {
+    width: "100%",
+  },
+  // Totals
+  totalsContainer: {
     padding: theme.spacing(4),
-    boxSizing: 'border-box'
+    textAlign: 'center',
+    borderTop: `2px solid ${theme.palette.primary.light}`,
+    '& p': {
+      fontSize: theme.spacing(4)
+    }
   }
 }));
 
@@ -47,214 +97,234 @@ const Basket = () => {
   const products = useContext(DashboardContext);
   const [shape, setShape] = useState({});
   const [items, setItems] = useState({});
-  const [total, setTotal] = useState([{}])
+  const [total, setTotal] = useState([]);
   const [availableColors, setAvailableColors] = useState([]);
-  const [selectedFilter, setSelectedFilter] = useState('all')
-
-  const [totalItemsValue, setTotalItemsValue] = useState([])
+  const [selectedFilter, setSelectedFilter] = useState("All");
+  const [bigTotal, setBigTotal] = useState(0)
 
   const calculateItemValue = (q) => {
+    const updatedItems = total.filter((x) => x.id !== q.id);
     setTotal([
-      ...total,
+      ...updatedItems,
       {
         id: q.id,
         value: q.value,
-        action: q.action
-      }
-    ])
-  }
+        action: q.action,
+        quantity: q.quantity,
+      },
+    ]);
+  };
 
   useEffect(() => {
-    console.log(total)
-  }, [total])
-
-  // useEffect(() => {
-  //   const data = total.reduce(
-  //     (results, current) => ({
-  //       ...total,
-  //       [current.id]: {
-  //         id: current.id,
-  //         value: current.value
-  //       }
-  //     })
-  //   )
-  //   setTotalItemsValue(data)
-
-  // }, [total])
-
-  // useEffect(() => {
-  //   console.log(totalItemsValue)
-  // }, [totalItemsValue])
-
-
-
-  // useEffect(() => {
-  //   // console.log(total)
-  //   // Object.values(total).map(i => console.log(i))
-
-  //   // Object.values(total).reduce(function (accumulator, currentValue) {
-  //   //   return (
-  //   //   )
-  //   // console.log(total)
-
-  //   function getIds() {
-  //     let result = [];
-  //     Object.values(total).map(i => result.push(i.id))
-  //     result = [...new Set(result)]
-  //     return result.filter(x => x !== undefined)
-  //   }
-
-  //   function updateValues() {
-  //     let result = [];
-  //     Object.values(total).map(i => result.push(i.value))
-  //     return result;
-  //   }
-
-  //   let listOfId = getIds();
-
-  //   listOfId.forEach(element => {
-  //     updateIds(element)
-  //   })
-
-  //   updateValues()
-
-  // }, [total])
-
-
+    const result = total.reduce((acc, cur) => {
+      return acc + cur.value * cur.quantity;
+    }, 0);
+    setBigTotal(result)
+  }, [total]);
 
   // Add data into state
   useEffect(() => {
     if (products.data !== undefined && products.data.length !== undefined) {
       const result = [];
-      products.data.map(item =>
-        result.push(
-          {
-            id: item.id,
-            colour: item.colour,
-            name: item.name,
-            price: item.price,
-            img: item.img,
-            quantity: 0
-          }
-        )
-      )
-      setShape(result)
+      products.data.map((item) =>
+        result.push({
+          id: item.id,
+          colour: item.colour,
+          name: item.name,
+          price: item.price,
+          img: item.img,
+          quantity: 0,
+        })
+      );
+      setShape(result);
     }
-  }, [products])
+  }, [products]);
 
-  // Dynamically add colors to state 
+  // Dynamically add colors to state
   useEffect(() => {
     const addColors = () => {
       let colors = [];
       let uniqueColors = [];
 
       if (shape !== undefined && shape.length !== undefined) {
-        shape.map(item =>
-          colors.push(
-            item.colour
-          )
-        )
+        shape.map((item) => colors.push(item.colour));
         uniqueColors = [...new Set(colors)];
       }
-      setAvailableColors(uniqueColors)
-    }
+      setAvailableColors(uniqueColors);
+    };
     addColors();
-  }, [shape])
+  }, [shape]);
 
   // Filter Data
   const filterItems = () => {
-
     const handleChange = (event) => {
       setSelectedFilter(event.target.value);
-    }
+    };
 
     if (availableColors.length !== undefined && availableColors.length > 0) {
       return (
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="age-native-simple">Colors</InputLabel>
-          <Select
-            native
-            value={selectedFilter}
-            onChange={handleChange}
-            inputProps={{
-              name: 'age',
-              id: 'age-native-simple',
-            }}
-          >
-            <option value='all' key='all'>All</option>
-            {availableColors.map(item => {
-              return (
-                <option value={item} key={item}>{item}</option>
-              )
-            })}
-          </Select>
-        </FormControl>
-      )
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <div className={classes.formControl}>
+              <FormControl className={classes.formControlItem}>
+                <InputLabel htmlFor="age-native-simple">Colors</InputLabel>
+                <Select
+                  native
+                  value={selectedFilter}
+                  onChange={handleChange}
+                  inputProps={{
+                    name: "age",
+                    id: "age-native-simple",
+                  }}
+                >
+                  <option value="All" key="all">
+                    All
+                </option>
+                  {availableColors.map((item) => {
+                    return (
+                      <option value={item} key={item}>
+                        {item}
+                      </option>
+                    );
+                  })}
+                </Select>
+              </FormControl>
+            </div>
+          </Grid>
+        </Grid>
+
+      );
     }
-  }
+  };
 
   // Re-render Data
   useEffect(() => {
     let result = [];
     if (shape !== undefined && shape.length !== undefined) {
-      if (selectedFilter === 'all') {
-        result.push(shape)
+      if (selectedFilter === "All") {
+        result.push(shape);
       } else {
-        result.push(shape.filter(x => x.colour === selectedFilter));
+        result.push(shape.filter((x) => x.colour === selectedFilter));
       }
-      setItems(result)
+      setItems(result);
     }
-  }, [shape, selectedFilter])
+  }, [shape, selectedFilter]);
 
   // Show items in a list
   const itemsLoop = () => {
-    let result = []
+    let result = [];
     if (items !== undefined && items.length !== undefined) {
-      items.map(i => {
-        i.map(item => (
+      items.map((i) => {
+        i.map((item) =>
           result.push(
-            <Grid container spacing={2} key={item.id} className={classes.itemContainer}>
-              <Grid item xs={4}>
-                <div className={classes.imageContainer}>
-                  <img src={item.img} alt={item.name} />
+            <Grid
+              container
+              spacing={2}
+              key={item.id}
+              className={classes.itemContainer}
+            >
+              <Grid item xs={9}>
+                <div className={classes.left}>
+                  <Grid
+                    container
+                    spacing={2}
+                    key={item.id}
+                    className={classes.itemContainer}
+                  >
+                    <Grid item xs={4}>
+                      <div className={classes.imageContainer}>
+                        <img src={item.img} alt={item.name} />
+                      </div>
+                    </Grid>
+                    <Grid item xs={8} className={classes.textContainer}>
+                      <Typography variant="h6">{item.name}</Typography>
+                      <Typography variant="h6">
+                        <span>&pound;</span>
+                        {item.price}
+                      </Typography>
+                    </Grid>
+                  </Grid>
                 </div>
               </Grid>
-              <Grid item xs={8} className={classes.textContainer}>
-                <Typography variant='h6'>
-                  {item.name}
-                </Typography>
-                <Typography variant='h6'>
-                  <span>&pound;</span>{item.price}
-                </Typography>
-                <Quantity itemId={item.id} itemPrice={item.price} updateCart={q => calculateItemValue(q)} />
+
+              <Grid item xs={3}>
+                <div className={classes.right}>
+                  <Quantity
+                    itemId={item.id}
+                    itemPrice={item.price}
+                    updateCart={(q) => calculateItemValue(q)}
+                  />
+                </div>
               </Grid>
+
             </Grid>
           )
-        ))
-        return result
-      }
-      )
+        );
+        return result;
+      });
     }
-    return result
+    return result;
+  };
+
+  // Round Digits
+  function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
   }
 
+  // Show Totals
+  const showTotals = () => {
+    return (
+      <div className={classes.totalsContainer}>
+        <Typography><span>&pound;</span>{round(bigTotal, 2)}</Typography>
+      </div>
+    )
+  }
 
   return (
-    <React.Fragment >
+    <React.Fragment>
       <div className={classes.root}>
+        <AppBar position="sticky">
+          <Toolbar>
+
+            <div className={classes.headerItems}>
+              <div className={classes.activeFilters}>
+                <Typography> Show Colors: <span>{selectedFilter}</span></Typography>
+              </div>
+              <div className={classes.totalsContainerHeader}>
+                Total: <span>&pound;{round(bigTotal, 2)}</span>
+              </div>
+            </div>
+
+          </Toolbar>
+        </AppBar>
         <Paper>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              {itemsLoop()}
+          <div className={classes.content}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <div className={classes.filterDropdown}>
+                  {filterItems()}
+                </div>
+                <div className={classes.itemsLoop}>
+                  {itemsLoop()}
+                </div>
+              </Grid>
+              <Grid item xs={12}>
+                <Grid
+                  container
+                  spacing={2}
+                >
+                  <Grid item xs={9}></Grid>
+                  <Grid item xs={3}>
+                    {showTotals()}
+                  </Grid>
+                </Grid>
+              </Grid>
             </Grid>
-            <Grid item xs={6}>
-              {filterItems()}
-            </Grid>
-          </Grid>
+          </div>
         </Paper>
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Basket;
